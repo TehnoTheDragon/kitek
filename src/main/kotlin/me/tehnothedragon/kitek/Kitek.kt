8c847cf75e057874ef99ca9fa38202cf8021d72d
TehnoTheDragon <tehnothedragon@gmail.com>
2025-11-24 02:51:47 +0200

    added simple mod entry/initialization
    currently has only ContentLoader calling it's start method

--- src/main/kotlin/me/tehnothedragon/kitek/Kitek.kt
+++ src/main/kotlin/me/tehnothedragon/kitek/Kitek.kt
@@ -1,9 +1,17 @@
 package me.tehnothedragon.kitek
 
+import me.tehnothedragon.kitek.content.ContentLoader
 import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class Kitek : ModInitializer {
+    companion object {
+        const val MODID: String = "kitek"
+        internal val logger: Logger = LoggerFactory.getLogger(MODID)
+    }
 
     override fun onInitialize() {
+        ContentLoader.start()
     }
 }
