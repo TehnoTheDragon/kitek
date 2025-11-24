ec37f86761f5ce63b8300612876bae42a9ce90d7
TehnoTheDragon <tehnothedragon@gmail.com>
2025-11-24 04:02:46 +0200

    added ContentLoader.kt
    it ensure that directory content exists in minecraft root folder
    
    method `start` called from main mod entry to load all content packs from content directory

---
+++ src/main/kotlin/me/tehnothedragon/kitek/content/ContentLoader.kt
@@ -0,0 +1,50 @@
+package me.tehnothedragon.kitek.content
+
+import me.tehnothedragon.kitek.Kitek
+import me.tehnothedragon.kitek.registry.KitekRegistries
+import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+import java.nio.file.FileVisitOption
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.isDirectory
+import kotlin.io.path.notExists
+
+object ContentLoader {
+    private fun ensureDirectory(path: Path): Path {
+        if (path.notExists()) {
+            Files.createDirectory(path)
+        }
+        return path
+    }
+
+    val GAME_DIR: Path = FabricLoader.getInstance().gameDir.normalize().toAbsolutePath()
+    val CONTENT_DIR: Path = ensureDirectory(GAME_DIR.resolve("content"))
+
+    private fun getContentPacks(): List<Path> {
+        return Files.walk(CONTENT_DIR, 1, FileVisitOption.FOLLOW_LINKS)
+            .filter { it.isDirectory() && it != CONTENT_DIR }
+            .toList()
+    }
+
+    private fun loadPacks() {
+        getContentPacks().forEach {
+            val contentPack = ContentPack.fromPackDir(it)
+            Registry.register(
+                KitekRegistries.CONTENT_PACK,
+                Identifier.of(Kitek.MODID, contentPack.metadata.id),
+                contentPack
+            )
+        }
+    }
+
+    private fun processPacks() {
+        KitekRegistries.CONTENT_PACK.forEach { ContentPackLoader(it).load() }
+    }
+
+    internal fun start() {
+        loadPacks()
+        processPacks()
+    }
+}
