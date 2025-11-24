58dcf2822bdc6a664bec45c62b80b43faed452ab
TehnoTheDragon <tehnothedragon@gmail.com>
2025-11-24 04:04:50 +0200

    refactored KitekRegistries.kt and KitekRegistryKeys.kt
    added method `create` for both
    
    added new Registry and RegistryKey for ContentType.kt

--- src/main/kotlin/me/tehnothedragon/kitek/registry/KitekRegistries.kt
+++ src/main/kotlin/me/tehnothedragon/kitek/registry/KitekRegistries.kt
@@ -1,12 +1,19 @@
 package me.tehnothedragon.kitek.registry
 
 import me.tehnothedragon.kitek.content.ContentPack
+import me.tehnothedragon.kitek.content.ContentType
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
 import net.fabricmc.fabric.api.event.registry.RegistryAttribute
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
 
 object KitekRegistries {
-    val CONTENT_PACK: Registry<ContentPack> = FabricRegistryBuilder.createSimple(KitekRegistryKeys.CONTENT_PACK)
-        .attribute(RegistryAttribute.SYNCED)
-        .buildAndRegister()
+    val CONTENT_PACK: Registry<ContentPack> = create(KitekRegistryKeys.CONTENT_PACK)
+    val CONTENT_TYPE: Registry<ContentType> = create(KitekRegistryKeys.CONTENT_TYPE)
+
+    private fun <T> create(registryKey: RegistryKey<Registry<T>>, attribute: RegistryAttribute = RegistryAttribute.SYNCED): Registry<T> {
+        return FabricRegistryBuilder.createSimple(registryKey)
+            .attribute(attribute)
+            .buildAndRegister()
+    }
 }
--- src/main/kotlin/me/tehnothedragon/kitek/registry/KitekRegistryKeys.kt
+++ src/main/kotlin/me/tehnothedragon/kitek/registry/KitekRegistryKeys.kt
@@ -2,10 +2,16 @@ package me.tehnothedragon.kitek.registry
 
 import me.tehnothedragon.kitek.Kitek
 import me.tehnothedragon.kitek.content.ContentPack
+import me.tehnothedragon.kitek.content.ContentType
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.util.Identifier
 
 object KitekRegistryKeys {
-    val CONTENT_PACK: RegistryKey<Registry<ContentPack>> = RegistryKey.ofRegistry(Identifier.of(Kitek.MODID, "content_pack"))
+    val CONTENT_PACK: RegistryKey<Registry<ContentPack>> = create("content_pack")
+    val CONTENT_TYPE: RegistryKey<Registry<ContentType>> = create("content_type")
+
+    private fun <T> create(type: String): RegistryKey<Registry<T>> {
+        return RegistryKey.ofRegistry(Identifier.of(Kitek.MODID, type))
+    }
 }
