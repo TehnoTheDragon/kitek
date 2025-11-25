8c9de1b572788f18ad9ce60b50889d1a909f7b74
TehnoTheDragon <tehnothedragon@gmail.com>
2025-11-25 02:13:25 +0200

    added ContentPackResourcePackProvider.kt
    It generates information about content pack's resource pack and registers it when requested

---
+++ src/main/kotlin/me/tehnothedragon/kitek/resource/ContentPackResourcePackProvider.kt
@@ -0,0 +1,59 @@
+package me.tehnothedragon.kitek.resource
+
+import me.tehnothedragon.kitek.Kitek
+import me.tehnothedragon.kitek.content.ContentPack
+import net.minecraft.registry.Registry
+import net.minecraft.resource.*
+import net.minecraft.text.Text
+import java.util.*
+import java.util.function.Consumer
+import java.util.function.Supplier
+
+class ContentPackResourcePackProvider(private val supplier: Supplier<Registry<ContentPack>>): ResourcePackProvider {
+    private fun createResourcePackProfileFor(contentPack: ContentPack): ResourcePackProfile {
+        val info = ResourcePackInfo(
+            contentPack.metadata.id,
+            Text.literal("${contentPack.metadata.name} Resources"),
+            ResourcePackSource.BUILTIN,
+            Optional.empty()
+        )
+
+        val resourcePack = ContentPackResourcePack(contentPack, info)
+
+        Kitek.logger.info("Created ResourcePackInfo $info")
+
+        val position = ResourcePackPosition(
+            true,
+            ResourcePackProfile.InsertionPosition.BOTTOM,
+            false,
+        )
+
+        val profile = ResourcePackProfile.create(
+            info,
+            ContentPackResourcePackFactory(resourcePack),
+            ResourceType.CLIENT_RESOURCES,
+            position
+        )!!
+
+        return profile
+    }
+
+    override fun register(consumer: Consumer<ResourcePackProfile>) {
+        supplier.get().forEach { contentPack ->
+            consumer.accept(createResourcePackProfileFor(contentPack))
+        }
+    }
+
+    class ContentPackResourcePackFactory(val resourcePack: ResourcePack): ResourcePackProfile.PackFactory {
+        override fun open(info: ResourcePackInfo): ResourcePack {
+            return this.resourcePack
+        }
+
+        override fun openWithOverlays(
+            info: ResourcePackInfo,
+            metadata: ResourcePackProfile.Metadata,
+        ): ResourcePack {
+            return this.resourcePack
+        }
+    }
+}
