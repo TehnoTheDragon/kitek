bd27fb72a9207b7aa53e0ad4f8e0be6e90e6544a
TehnoTheDragon <tehnothedragon@gmail.com>
2025-11-25 04:18:09 +0200

    added KitekTestMod

---
+++ src/test/kotlin/me/tehnothedragon/client/kitek_test_mod/KitekTestModClient.kt
@@ -0,0 +1,8 @@
+package me.tehnothedragon.client.kitek_test_mod
+
+import net.fabricmc.api.ClientModInitializer
+
+class KitekTestModClient: ClientModInitializer {
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/test/kotlin/me/tehnothedragon/kitek_test_mod/KitekTestMod.kt
@@ -0,0 +1,39 @@
+package me.tehnothedragon.kitek_test_mod
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import me.tehnothedragon.kitek.content.CodecContentType
+import me.tehnothedragon.kitek.content.ContentPack
+import me.tehnothedragon.kitek.content.ContentType
+import me.tehnothedragon.kitek.registry.KitekRegistries
+import net.fabricmc.api.ModInitializer
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+import org.slf4j.LoggerFactory
+
+class KitekTestMod: ModInitializer {
+    override fun onInitialize() {
+        Registry.register(KitekRegistries.CONTENT_TYPE, Identifier.of("kitek_test_mod", "item"), ItemContentType())
+    }
+
+    class ItemContentType: CodecContentType<ItemContentType.Data> {
+        override val codec: Codec<Data> = RecordCodecBuilder.create { builder ->
+            builder.group(
+                Codec.INT.fieldOf("maxStack").forGetter(Data::maxStack)
+            ).apply(builder, ::Data)
+        }
+
+        override fun process(pack: ContentPack, dataHolder: ContentType.DataHolder<Data>) {
+            LoggerFactory.getLogger("kitek.test.mod").info("Register Item ${pack.metadata.id}:${dataHolder.name}")
+            Registry.register(
+                Registries.ITEM,
+                pack.getIdentifierFor(dataHolder.name),
+                Item(Item.Settings().maxCount(dataHolder.data.maxStack))
+            )
+        }
+
+        class Data(val maxStack: Int)
+    }
+}
